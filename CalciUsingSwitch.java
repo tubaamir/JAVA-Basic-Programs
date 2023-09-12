190caeb2023566b19519548808129c671bf4d4ed
tubaamir <135152116+tubaamir@users.noreply.github.com>
2023-09-12 19:30:48 +0530

    Update CalciUsingSwitch.java

--- CalciUsingSwitch.java
+++ CalciUsingSwitch.java
@@ -1,19 +1,23 @@
+//This is a simple calculator using Switch case  
+
 import java.util.Scanner;
 
 public class CalciUsingSwitch {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        //input numbers
         System.out.println("Enter first number:");
         int a = sc.nextInt();
-
         System.out.println("Enter second number:");
         int b = sc.nextInt();
 
+        //input operator
         System.out.println("Enter your choice:");
         System.out.println("1 for Addition\n2 for Subtraction\n3 for Multiplication\n4 for Divide");
         int opp = sc.nextInt();
 
+        //switch cases
         switch (opp) {
             case 1 : int add = a + b ;
             System.out.println("Addition of "+a+" and "+b+" are "+add);
