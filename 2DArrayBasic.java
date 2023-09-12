33504083efbbabbf40bde5942418c5ce6389e821
tubaamir <135152116+tubaamir@users.noreply.github.com>
2023-09-12 19:26:54 +0530

    Update 2DArrayBasic.java

--- 2DArrayBasic.java
+++ 2DArrayBasic.java
@@ -1,20 +1,24 @@
+//This program is of 2D array
+
 import java.util.*;
 public class 2DArrayBasic {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        //input size, rows and column
         System.out.print("Enter the size of Matrix = ");
         int row = sc.nextInt();
         int col = sc.nextInt();
         int matrix[][] = new int[row][col];
-        //input
+        
+        //input values of array
         System.out.println("Enter the values ");
         for (int i=0; i<row; i++) {
             for (int j=0; j<col; j++) {
-                //System.out.println("value at index "+i+" "+j+" = ");
                 matrix[i][j] = sc.nextInt();
             }
         }
-        //output
+        
+        //printing values of array
         System.out.println("matrix is = ");
         for (int i=0; i<row; i++) {
             for (int j=0; j<col; j++) {
